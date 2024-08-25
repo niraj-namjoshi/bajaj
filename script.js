3221ef811cd6ed13d5e446c05693eb2f16b6c096
niraj-namjoshi <100368161+niraj-namjoshi@users.noreply.github.com>
2024-08-25 12:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.getElementById('submit-btn').addEventListener('click', function() {
         }
 
 
-        fetch('http://127.0.0.1:5000/bfhl', {
+        fetch('https://nirajnn.pythonanywhere.com/bfhl', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
